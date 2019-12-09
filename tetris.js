7dbb3fb972b49ffdbdfee6c0e36798ebfee0257a
Vlad Purcarea <vpurcarea@simscale.com>
2019-12-09 04:04:21 +0100

    test pixijs

--- tetris.js
+++ tetris.js
@@ -1,2 +1,20 @@
-let app = new PIXI.Application({width: 420, height: 600});
-document.body.appendChild(app.view)
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
+let app = new Application({width: 420, height: 600});
+document.body.appendChild(app.view);
+
+loader
+    .add("assets/black.png")
+    .add("assets/dark_blue.png")
+    .add("assets/real_black.png")
+    .add("assets/white.png")
+    .load(setup);
+
+function setup() {
+    let white = new Sprite(resources["assets/black.png"].texture);
+    app.stage.addChild(white)
+}
+
